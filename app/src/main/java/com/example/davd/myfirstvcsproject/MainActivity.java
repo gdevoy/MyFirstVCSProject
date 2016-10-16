15465bcf22e7205e7f4a16558c2784bf475d6b2d
gdevoy <david@gdevoy.freeserve.co.uk>
2016-10-16 18:25:46 +0100

    Changes based of F's suggestions

--- app/src/main/java/com/example/davd/myfirstvcsproject/MainActivity.java
+++ app/src/main/java/com/example/davd/myfirstvcsproject/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity {
     long startTime, endTime;
 
     MediaPlayer[] mySound = new MediaPlayer[10];
-    int select = 0;
-    int pause_on=1;
+    int select = 0, pause_on = 1, replay = 0;
     TextView opt, optT;
     String ans;
 
@@ -51,15 +50,28 @@ public class MainActivity extends AppCompatActivity {
 
     public void playMusic(View view){
         //set select to rand 0 to 8
-        if (pause_on==1) {
+        if (pause_on==1)                // set a no pause state
             pause_on = 0;
-            select = (int) (Math.random() * 8);
-            startTime = System.currentTimeMillis();
-            mySound[select].start();
+            if (replay == 0) {
+                select = (int) (Math.random() * 8);
+                startTime = System.currentTimeMillis();
+                mySound[select].start();
+                replay++;
+                optT.setText("You can press play to restart ");
 
-            opt.setText("Press PAUSE, select the tune then press CHOOSE");
+            }
+            else if(replay==1||replay==2){
+                mySound[select].start();
+                replay++;
+                optT.setText("You can press play to restart");
+            }
+             else {
+                replay = 0;
+                optT.setText("Press PLAY again for another tune ");
+
+            }
+                opt.setText("Press PAUSE, select the tune then press CHOOSE");
         }
-    }
 
     public void pauseMusic (View v) {
         if (mySound[select].isPlaying()) {
@@ -70,42 +82,53 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
-
-
+    private void delay(long d1, long d2) {  // method to calculate and output the delay
+        long startTime = d1;
+        long endTime = d2;
+        long delay = endTime-startTime;
+        double delayf = (double)delay;
+        delayf=delayf/1000.0;               // convert ms to seconds
+        optT.setText("You did that in "+Double.toString(delayf)+"s");
+        replay=0;
+    }
 
 
     public void chooseMusic (View v) {
 
         RadioButton choice = (RadioButton) findViewById(selector.getCheckedRadioButtonId());
         String ans = choice.getText().toString();
-        String OK="Correct, PLAY again?";
-        String TD="Wrong, PLAY again?";
+        String OK="Correct, PLAY another?";
+        String TD="Wrong!!!";
 
         if(pause_on==1) {
             if (ans.equals("Axelf") && select == 0) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("Under The Boardwalk") && select == 1) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("Pianoman") && select == 2) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("Alone") && select == 3) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("The Hollywood Waltz") && select == 4) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("Let The River Run") && select == 5) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("Saturday Night") && select == 6) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else if (ans.equals("Take It To The Limit") && select == 7) {
                 opt.setText(OK);
+                delay(startTime,endTime);
             } else {
                 opt.setText(TD);
             }
-            long delay = endTime-startTime;
-            double delayf = (double)delay;
-            delayf=delayf/1000.0;               // convert ms to seconds
-            optT.setText("You did that in "+Double.toString(delayf)+"s");
+
 
         }
     }
